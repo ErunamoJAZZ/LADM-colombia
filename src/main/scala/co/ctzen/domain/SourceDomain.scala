030b5dd8f34e738c60560a104195ced5b974ed9c
C. Daniel Sanchez R <cdsanchez@ctzen.co>
2016-09-05 12:14:00 -0500

    es un object

--- src/main/scala/co/ctzen/domain/SourceDomain.scala
+++ src/main/scala/co/ctzen/domain/SourceDomain.scala
@@ -3,7 +3,7 @@ package co.ctzen.domain
 /**
   * Created by cdsanchez on 5/09/16.
   */
-class SourceDomain {
+object SourceDomain {
 
   type COL_TransaccionRegistralTipo = COL_TransaccionRegistralTipo.Value
   type COL_AvailabilityStatusType = COL_AvailabilityStatusType.Value
